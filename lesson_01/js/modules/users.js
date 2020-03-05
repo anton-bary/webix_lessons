6e116936e4e275fb9bf3ab90c39cb30287aa4ed3
Anton <antonb.atwork@gmail.com>
2020-03-05 13:44:19 +0300

    feat: add editing posibility for users list

--- lesson_01/js/modules/users.js
+++ lesson_01/js/modules/users.js
@@ -1,6 +1,13 @@
+webix.protoUI({
+    name: "editlist"
+}, webix.EditAbility, webix.ui.list);
+
 var users = {
     rows: [{
-            view: "list",
+            view: "editlist",
+            editable: true,
+            editor: "text",
+            editValue: "name",
             id: "user_list",
             select: true,
             url: "js/users.js",
